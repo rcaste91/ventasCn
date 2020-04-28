4ccc34841d8f8cf92667192d99edd1ffd442b678
Ronald Castellon <rcaste91@gmail.com>
2020-04-28 14:28:32 -0600

    dockerfile y environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDtQtwVMYWBLN4fDDRIQIyZFksnSF8ETZo",
+    authDomain: "cnventasangular.firebaseapp.com",
+    databaseURL: "https://cnventasangular.firebaseio.com",
+    projectId: "cnventasangular",
+    storageBucket: "cnventasangular.appspot.com",
+    messagingSenderId: "252863004371",
+    appId: "1:252863004371:web:0c47347ede3465b4ce9e26",
+    measurementId: "G-ZW89JV8Z79"
+  }
 };
