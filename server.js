44f7661a936656833129edb99a2cabac954500ce
Ronald Castellon <rcaste91@gmail.com>
2020-09-03 11:26:12 -0600

    actualizando server.js para apuntar a public y no dist

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/ventasCN'));
+app.use(express.static('./public'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/ventasCN/'}
+    res.sendFile('index.html', {root: 'public/'}
   );
   });
 
